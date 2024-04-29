7e7dbe34e77e3985fb876de80e95ade083096f3b
Roger Franco <rogerfrancogalt@gmail.com>
2024-04-29 11:30:17 -0400

    Adicionada paginação por slide

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ import {SlideNav} from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init()
-console.log(slide);
+slide.addControl()
+// console.log(slide);
 // slide.changeSlide(1)
 // slide.activeNextSlide()
 slide.addArrow('.prev', '.next')
